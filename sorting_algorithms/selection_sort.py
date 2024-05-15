b8277ce3fe90816d9e02fea6327fdc1057e6bab4
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-14 21:33:08 -0500

    No need to loop for last item

--- sorting_algorithms/selection_sort.py
+++ sorting_algorithms/selection_sort.py
@@ -3,7 +3,7 @@ class SelectionSort:
     @staticmethod
     def sort(A: list):
         n = len(A)
-        for i in range(n):
+        for i in range(n - 1):
             minimum = A[i]
             index = i
             for j in range(i + 1, n):
