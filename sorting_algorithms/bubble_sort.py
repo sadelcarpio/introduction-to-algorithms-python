1c1d8bde8dcd2e3cbd9b188f7e9d5511a33ff8a4
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-17 17:05:56 -0500

    Bubble Sort

---
+++ sorting_algorithms/bubble_sort.py
@@ -0,0 +1,14 @@
+class BubbleSort:
+
+    @staticmethod
+    def sort(a: list, n: int):
+        for i in range(n):
+            for j in range(n - 1, i, -1):
+                if a[j] < a[j - 1]:
+                    a[j], a[j - 1] = a[j - 1], a[j]
+
+
+if __name__ == '__main__':
+    arr = [5, 2, 4, 6, 1, 3]
+    BubbleSort().sort(a=arr, n=len(arr))
+    print(arr)
