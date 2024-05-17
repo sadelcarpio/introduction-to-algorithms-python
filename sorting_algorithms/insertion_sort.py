42bc08f03c052b1acae3c8ce777f71e075c2728f
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-16 22:35:30 -0500

    Recursive Insertion Sort

--- sorting_algorithms/insertion_sort.py
+++ sorting_algorithms/insertion_sort.py
@@ -9,10 +9,20 @@ class InsertionSort:
                 a[j + 1] = a[j]
                 j = j - 1
             a[j + 1] = key
-        return a
+
+    @staticmethod
+    def recursive_sort(a: list, n: int):
+        if n == 0:
+            return
+        InsertionSort.recursive_sort(a, n - 1)
+        key = a[n - 1]
+        for i in range(n - 2, -1, -1):
+            if key < a[i]:
+                a[i + 1] = a[i]
+                a[i] = key
 
 
 if __name__ == "__main__":
     arr = [5, 2, 4, 6, 1, 3]
-    InsertionSort().sort(a=arr)
+    InsertionSort().recursive_sort(a=arr, n=len(arr))
     print(arr)
