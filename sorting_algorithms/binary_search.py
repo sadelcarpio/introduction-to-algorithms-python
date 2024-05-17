eec5ff47a8ef88450d5776cdb2e257434eb63026
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-17 00:38:14 -0500

    refactor

--- sorting_algorithms/binary_search.py
+++ sorting_algorithms/binary_search.py
@@ -1,11 +1,11 @@
 def binary_search(a: list, val: int, p: int, r: int):
-    if p > r:
+    if p >= r:
         return None
     q = int((p + r) / 2)
     if val == a[q]:
         return q
-    elif val < a[q]:
-        return binary_search(a, val, p, q - 1)
+    if val < a[q]:
+        return binary_search(a, val, p, q)
     else:
         return binary_search(a, val, q + 1, r)
 
@@ -14,5 +14,5 @@ if __name__ == '__main__':
     arr = [6, 5, 7, 3, 2, 1]
     arr.sort()
     print(arr)
-    index = binary_search(a=arr, val=4, p=0, r=5)
+    index = binary_search(a=arr, val=4, p=0, r=6)
     print(index)
